82be924b762804558828b213c89c707012c269b8
Alex Szczuczko <aszczucz@redhat.com>
2014-10-06 16:04:29 +0100

    Fix problems in main wrappers found by pylint

--- moromi.py
+++ moromi.py
@@ -11,25 +11,25 @@ default_config_paths = ["images.cfg"]
 boolean_config_options = ["commit"]
 
 def sc_build(args):
-    config = nagoya.cfg.read_config(args.config, default_config_paths, boolean_config_options)
+    config = nagoya.cli.cfg.read_config(args.config, default_config_paths, boolean_config_options)
     return nagoya.moromi.build_images(config, args.images, args.quiet_build)
 
 def scargs_build(parser):
     parser.add_argument("-b", "--quiet-build", action="store_true", help="Do not print the builds' stdout/stderr")
     imgs = parser.add_argument("images", metavar="IMAGE", nargs="+", help="Image to build")
-    if nagoya.args.argcomplete_available:
-        imgs.completer = nagoya.args.ConfigSectionsCompleter(default_config_paths)
+    if nagoya.cli.args.argcomplete_available:
+        imgs.completer = nagoya.cli.args.ConfigSectionsCompleter(default_config_paths)
 
 def sc_clean(args):
     pass
 
 if __name__ == "__main__":
-    parser = nagoya.args.create_default_argument_parser(description="Build docker images")
-    nagoya.args.add_subcommand_subparsers(parser)
-    nagoya.args.attempt_autocomplete(parser)
+    parser = nagoya.cli.args.create_default_argument_parser(description="Build docker images")
+    nagoya.cli.args.add_subcommand_subparsers(parser)
+    nagoya.cli.args.attempt_autocomplete(parser)
     args = parser.parse_args()
 
-    nagoya.log.setup_logger(args.quiet, args.verbose)
+    nagoya.cli.log.setup_logger(args.quiet, args.verbose)
 
-    nagoya.args.run_subcommand_func(args, parser)
+    nagoya.cli.args.run_subcommand_func(args, parser)
 
--- toji.py
+++ toji.py
@@ -11,30 +11,30 @@ default_config_paths = ["containers.cfg"]
 boolean_config_options = ["multiple", "detach", "run_once"]
 
 def _config_dict(args):
-    return nagoya.cfg.read_config(args.config, default_config_paths, boolean_config_options)
+    return nagoya.cli.cfg.read_config(args.config, default_config_paths, boolean_config_options)
 
 def sc_init(args):
-    toji = Toji.from_dict(_config_dict(args))
+    toji = nagoya.toji.Toji.from_dict(_config_dict(args))
     toji.init_containers()
 
 def sc_start(args):
-    toji = Toji.from_dict(_config_dict(args))
+    toji = nagoya.toji.Toji.from_dict(_config_dict(args))
     toji.start_containers()
 
 def sc_stop(args):
-    toji = Toji.from_dict(_config_dict(args))
+    toji = nagoya.toji.Toji.from_dict(_config_dict(args))
     toji.stop_containers()
 
 def sc_remove(args):
-    toji = Toji.from_dict(_config_dict(args))
+    toji = nagoya.toji.Toji.from_dict(_config_dict(args))
     toji.remove_containers()
 
 if __name__ == "__main__":
-    parser = nagoya.args.create_default_argument_parser(description="Manage Koji Docker container systems")
-    nagoya.args.add_subcommand_subparsers(parser)
-    nagoya.args.attempt_autocomplete(parser)
+    parser = nagoya.cli.args.create_default_argument_parser(description="Manage Koji Docker container systems")
+    nagoya.cli.args.add_subcommand_subparsers(parser)
+    nagoya.cli.args.attempt_autocomplete(parser)
     args = parser.parse_args()
 
-    nagoya.log.setup_logger(args.quiet, args.verbose)
+    nagoya.cli.log.setup_logger(args.quiet, args.verbose)
 
-    nagoya.args.run_subcommand_func(args, parser)
+    nagoya.cli.args.run_subcommand_func(args, parser)
