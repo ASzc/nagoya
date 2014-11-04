931c3b584bcade0166df3521021414d144dea052
Alex Szczuczko <aszczucz@redhat.com>
2014-11-04 13:59:04 +0000

    Modify mock to use Docker's sys vfs, since mount is restricted inside containers

--- cfg/koji-builder/setup.py
+++ cfg/koji-builder/setup.py
@@ -49,6 +49,34 @@ with cfg.mod_text("/usr/lib/python2.6/site-packages/mockbuild/util.py") as lines
     i = lines.index("def unshare(flags):\n")
     lines.insert(i+1, "    return\n")
 
+log.info("Modify mock to use Docker's system vfs mounts")
+with cfg.mod_text("/usr/lib/python2.6/site-packages/mockbuild/mounts.py") as lines:
+    dict_ins_index = lines.index("class FileSystemMountPoint(MountPoint):\n")
+    lines.insert(dict_ins_index-1, """
+type_paths = {
+    "proc" : "/proc"
+    "sysfs" : "/sys"
+    "tmpfs" : "/dev/shm"
+    "devpts" : "/dev/pts"
+}
+""")
+    mount_ins_index = lines.index("    def mount(self):\n")
+    lines.insert(dict_ins_index+3, """
+        if self.filetype in type_paths:
+            link_from = type_paths[self.filetype]
+            mockbuild.util.rmtree(self.path)
+            os.symlink(link_from, self.path)
+        self.mounted = True
+        return True
+""")
+    mount_ins_index = lines.index("    def umount(self, force=False, nowarn=False):\n")
+    lines.insert(dict_ins_index+3, """
+        if self.filetype in type_paths:
+            os.remove(self.path)
+        self.mounted = False
+        return True
+""")
+
 #
 # Koji CLI
 #
